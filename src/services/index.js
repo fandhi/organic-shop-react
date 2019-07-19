58748e8521421e33f9b3aa28c9c96e909f0387c6
Safandhi Yonathan <safandhi.yonathan@elevenia.co.id>
2019-07-19 18:11:26 +0700

    separate files global API

--- src/services/index.js
+++ src/services/index.js
@@ -1,33 +1,8 @@
 import axios from 'axios';
+import Post from './Post';
+import Get from './Get';
 
-const RootPath = 'http://localhost:3004';
-const OnlineRoot = 'http://jsonplaceholder.typicode.com';
 
-const Get = (path, root) => {
-    const promise = new Promise((resolve, reject) => {
-        axios.get(`${root ? OnlineRoot : RootPath}/${path}`)
-            .then(response => {
-                //   console.log('axios ',response)
-                resolve(response.data)
-            }, (err) => {
-                reject(err);
-            })
-    })
-    return promise;
-}
-
-const Post = (path, root, data) => {
-    const promise = new Promise((resolve, reject) => {
-        axios.post(`${root ? OnlineRoot : RootPath}/${path}`, data)
-            .then(response => {
-                resolve(response.data)
-            }, (err) => {
-                reject(err);
-                console.log('error ', err)
-            })
-    })
-    return promise;
-}
 
 const postNewsBlog = (data) => Post('posts', false, data);
 
